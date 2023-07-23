e8a82d04ccc9767671d0d6b4ce5eb0d70b2dd245
Pedro Lucas Carolino Felix <pedro.carolino.develop@gmail.com>
2023-07-23 18:16:30 -0300

    criando uma nav para segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.avif";
 
 function App() {
+  const img = logo;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="App-nav">
+        <div>
+          <img src={img} alt="Minha logo" className="App-logo"></img>
+        </div>
+
+        <div className="App-navItens">
+          <ul className="App-navItens-ul">
+            <li className="App-navItens-ul-li">
+              <a href="#">Inicio</a>
+            </li>
+            <li className="App-navItens-ul-li">
+              <a href="#">Catálogo</a>
+            </li>
+            <li className="App-navItens-ul-li">
+              <a href="#">Suporte</a>
+            </li>
+            <li className="App-navItens-ul-li">
+              <a href="#">Carrinho</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
